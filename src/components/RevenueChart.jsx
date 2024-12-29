90ef0b27e78d388f81c8d68d8c140ecc4b758ff1
anhdt2 <anhdt2@tnteco.vn>
2024-12-29 21:12:15 +0700

    update

--- src/components/RevenueChart.jsx
+++ src/components/RevenueChart.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Button, DatePicker, Select, Space } from 'antd';
+import { Button, DatePicker, Select, Space, Table } from 'antd';
 import dayjs, { Dayjs } from 'dayjs';
 import { Line } from '@ant-design/plots';
 import server from 'constant/linkapi';
@@ -10,18 +10,22 @@ const RevenueChart = (revenueData3) => {
   const [statisticOption, setStatisticOption] = useState("revenue");
   const [date, setDate] = useState([dayjs('2024-01-01'), dayjs('2024-12-31')]);
   const [data, setData] = useState([]);
+  const [productVariants, setProductVariants] = useState([]);
   const [load, setLoad] = useState(true);
   useEffect(() => {
     let linkAPI = server + `${statisticOption == "revenue" ? "/payment/statistic/revenue" : "/order/statistic/sold"}?from=${date[0].startOf(frequency.toLowerCase()).valueOf()}&to=${date[1].endOf(frequency.toLowerCase()).valueOf()}&frequency=${frequency}`;
     const callAPI = async () => {
+      const productVariantsResponse = await axios.get(server + "/order/statistic/best-sales");
+      setProductVariants(productVariantsResponse.data.data);
       const response = await axios.get(linkAPI);
       const dataResponse = response.data.data.data;
       setData(dataResponse)
       console.log(response.data.data.data);
-
     }
     callAPI();
   }, [load])
+  console.log(productVariants);
+
   const frequencyOption = [
     {
       label: "Tháng",
@@ -104,6 +108,38 @@ const RevenueChart = (revenueData3) => {
       <Button type='primary' onClick={() => setLoad(!load)}>Thống kê</Button>
     </Space>
     {data.length == 0 ? <div style={{ padding: "20px 0" }}>Không có dữ liệu</div> : <Line {...config} />}
+    <h5>TOP 5 Sản phẩm bán chạy nhất</h5>
+    <table>
+      <thead>
+        <tr>
+          <th>Số thứ tự</th>
+          <th className='text'>Mã sản phẩm phân loại</th>
+          <th>Thuộc sản phẩm Id</th>
+          <th>SkuCode</th>
+          <th>Giá</th>
+          <th>Số lượng</th>
+          <th>Kích cỡ</th>
+          <th>Số lượt bán ra</th>
+        </tr>
+      </thead>
+      <tbody>
+        {productVariants?.map((item, index) => {
+          return (
+            <tr key={item.id}>
+              <td>{index + 1}</td>
+              <td>{item.id}</td>
+              <td>{item.productId}</td>
+              <td>{item.skuCode}</td>
+              <td>{item.price.toLocaleString() + " đ"}</td>
+              <td>{item.stock}</td>
+              <td>{item.attributes["kích cỡ"]}</td>
+              <td>{item.sold}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+
   </>
 };
 
